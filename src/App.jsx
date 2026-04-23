661cce2f398cb7460c18b72a983bd5113e1aa797
koushikzlitx-pixel <koushikzlitx@gmail.com>
2026-04-24 02:58:45 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Supply Chain Time Cycle & Bottleneck Analyzer</h1>
+      <p>Welcome to the frontend application.</p>
+    </div>
+  )
+}
+
+export default App
